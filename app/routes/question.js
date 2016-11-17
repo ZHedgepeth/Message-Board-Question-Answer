1997ccbb8e2c0898d8a3096f5d05ae9d6ec81626
zhedgepeth <zhedgepeth1124@gmail.com>
2016-11-16 21:49:50 -0500

    install ember bootstrap

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,4 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params){
+    return Ember.RSVP.hash({
+      question: this.store.findRecord('question', params.question_id),
+    });
+  },
+
+  actions: {
+    update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('index');
+    },
+    destroyQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    },
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function(){
+        return question.save();
+      });
+    }
+  }
 });
