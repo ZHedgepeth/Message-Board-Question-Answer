b67eaf739474f44866cba41850d6ba3f014d99ce
zhedgepeth <zhedgepeth1124@gmail.com>
2016-11-16 20:00:30 -0500

    add properties for models

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  author: DS.attr(),
+  answerContent: DS.attr(),
+  votes: DS.attr(),
+  question: DS.belongsTo('question', {async: true})
 });
--- app/models/question.js
+++ app/models/question.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  author: DS.attr(),
+  title: DS.attr(),
+  questionContent: DS.attr(),
+  answers: DS.hasMany('answer', {async: true})
 });
