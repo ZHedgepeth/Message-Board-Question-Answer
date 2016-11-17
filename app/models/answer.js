f60006db1815e81d9681edaa670f2233774e6320
zhedgepeth <zhedgepeth1124@gmail.com>
2016-11-16 21:27:01 -0500

    add favorites functionality but still not working

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,6 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   author: DS.attr(),
   answerContent: DS.attr(),
-  votes: DS.attr(),
   question: DS.belongsTo('question', {async: true})
 });
